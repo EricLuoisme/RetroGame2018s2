abeee8bf0be5228c24796dbca5c6e9640ab573c1
Wenjing Xue <u6269409@anu.edu.au>
2018-10-16 23:16:34 +1100

    Make the app full screen

--- app/src/main/java/com/example/jiangxinwei/pacman/GameActivity.java
+++ app/src/main/java/com/example/jiangxinwei/pacman/GameActivity.java
@@ -3,6 +3,8 @@ package com.example.jiangxinwei.pacman;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity implements GameOver {
@@ -12,6 +14,10 @@ public class GameActivity extends AppCompatActivity implements GameOver {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
         pacmanView = (PacmanView) findViewById(R.id.pacmanView);
         pacmanView.registerGameOver(this);
