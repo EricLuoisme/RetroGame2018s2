b5a0d6874b1a28a0b38b70a05754fe2c6beb8760
Wenjing Xue <u6269409@anu.edu.au>
2018-10-17 11:45:21 +1100

    add explain comments to chases file

--- app/src/main/java/com/example/jiangxinwei/pacman/Chasers.java
+++ app/src/main/java/com/example/jiangxinwei/pacman/Chasers.java
@@ -20,6 +20,9 @@ public class Chasers extends ArrayList<Chaser> {
 
     Random random = new Random();
 
+    /**
+     * This step() helps the chasers to take a random walk every step
+     */
     public void step() {
 
         if (this.size() < CHASERNUM) {
@@ -59,6 +62,14 @@ public class Chasers extends ArrayList<Chaser> {
         for (Chaser c : this) c.draw(canvas, paint, b);
     }
 
+    /**
+     * This helps to judge the next step of each chaser
+     * it will first sign the four direction as possible move
+     * and judge whether they are next valid move
+     * it will return the list of the next valid move
+     * @param chaser
+     * @return A list of the next valid move of each chaser
+     */
     private ArrayList<String> validMove(Chaser chaser){
         ArrayList<String> position = new ArrayList<>();
         position.add("Left");
@@ -92,6 +103,12 @@ public class Chasers extends ArrayList<Chaser> {
     }
 
 
+    /**
+     * The chaser gives the next possible move position to this function
+     * it return whether the position is out boundary
+     * @param p
+     * @return true/false
+     */
     private boolean outBoundary(Pos p){
         if(p.x < CHASERMINX || p.x > CHASERMAXX){
             return true;
@@ -102,6 +119,12 @@ public class Chasers extends ArrayList<Chaser> {
         return false;
     }
 
+    /**
+     * The chaser gives the next possible up/down move position to this function
+     * it return whether there is a Vertical wall between the current position and the next possible position
+     * @param p
+     * @return true/false
+     */
     private boolean hitVerticWalls(Pos p){
         for(Wall wall: Game.wallsVertic){
             if(Math.abs(p.x - wall.pos.x) <= 0.01 && Math.abs(p.y - wall.pos.y - 0.1) <= 0.01 ){
@@ -111,6 +134,12 @@ public class Chasers extends ArrayList<Chaser> {
         return false;
     }
 
+    /**
+     * he chaser gives the next possible left/right move position to this function
+     * it return whether there is a Horizon wall between the current position and the next possible position
+     * @param p
+     * @return true/false
+     */
     private boolean hitHorizonWalls(Pos p){
         for(Wall wall: Game.wallsHorizon){
             if(Math.abs(p.y - wall.pos.y) <=0.01 && Math.abs(p.x - wall.pos.x - 0.05) <= 0.01){
